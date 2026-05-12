43997eb97f12553c9b3102fb828ac45d3d8872e9
caadjo <cvd@aluno.ifnmg.edu.br>
2026-05-12 09:18:29 -0300

    feat(management): criar viewset admin de usuarios

--- management/views.py
+++ management/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from users.models import CustomUser
+from users.serializers import UserSerializer
+from users.permissions import IsAdmin
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+	queryset = CustomUser.objects.all().order_by('id')
+	serializer_class = UserSerializer
+	permission_classes = [IsAdmin]
+	http_method_names = ['get', 'post', 'put', 'patch', 'head', 'options']
