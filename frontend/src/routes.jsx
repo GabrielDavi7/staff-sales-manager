1c570268e38ce0b4346370b8a8dbb4a6076aa579
lucasrib421 <78276872+lucasrib421@users.noreply.github.com>
2026-08-01 16:30:46 -0300

    Readicionado Routes.jsx
    Foi excluido acidentalmente

---
+++ frontend/src/routes.jsx
@@ -0,0 +1,64 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import DashboardLayout from "./pages/dashboard/DashboardLayout";
+import { Home } from "./pages/dashboard/Home";
+import Login from "./pages/login/Login";
+import PrivateRoute from "./routes/PrivateRoute";
+import RoleRoute from "./routes/RoleRoute";
+import NewAttendance from "./pages/dashboard/NewAttendance";
+import Grafics from "./pages/dashboard/Grafics";
+import Team from "./pages/dashboard/Team";
+import AdminPainel from "./pages/admin/AdminPainel";
+import Perfil from "./pages/dashboard/perfil"; // Import corrigido para Maiúsculo
+
+export const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/",
+    element: (
+      <PrivateRoute>
+        <DashboardLayout />
+      </PrivateRoute>
+    ),
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "registrarVenda",
+        element: <NewAttendance />,
+      },
+      {
+        path: "funcionarios",
+        element: <Team />,
+      },
+      {
+        path: "graficos",
+        element: <Grafics />,
+      },
+      {
+        path: "meuperfil",
+        element: (
+          <RoleRoute allowedRoles={["VENDEDOR", "ADMIN", "SUPERVISOR"]}>
+            <Perfil />
+          </RoleRoute>
+        ),
+      },
+      {
+        path: "adminpainel",
+        element: (
+          <RoleRoute allowedRoles={["ADMIN"]}>
+            <AdminPainel />
+          </RoleRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <Navigate to="/" replace />,
+  },
+]);
