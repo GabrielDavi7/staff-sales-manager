78cba55d9c89dfa04f296e8bf79a424f09cc7cff
lucasrib421 <78276872+lucasrib421@users.noreply.github.com>
2026-08-02 19:06:59 -0300

    Corrigindo atributos do Painel de Adminitração Django

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
 from .models import Loja, Equipe, Metrica, Relatorio
 
-admin.site.register(Loja)
-admin.site.register(Equipe)
-admin.site.register(Metrica)
-admin.site.register(Relatorio)
+
+@admin.register(Loja)
+class LojaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'cidade', 'cliente', 'ativo']
+    list_filter = ['ativo', 'cliente']
+    search_fields = ['nome', 'cidade']
+
+
+@admin.register(Equipe)
+class EquipeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'loja', 'ativo']
+    list_filter = ['ativo', 'loja']
+    search_fields = ['nome']
+
+
+@admin.register(Metrica)
+class MetricaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'cliente', 'loja', 'ativo']
+    list_filter = ['ativo', 'cliente']
+    search_fields = ['nome']
+
+
+@admin.register(Relatorio)
+class RelatorioAdmin(admin.ModelAdmin):
+    list_display = ['id', 'data_hora', 'vendedor', 'venda_fechada', 'valor_venda', 'metrica']
+    list_filter = ['venda_fechada', 'data_hora']
+    search_fields = ['vendedor__username', 'cliente_nome']
+    date_hierarchy = 'data_hora'
